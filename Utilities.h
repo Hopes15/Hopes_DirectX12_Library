9328b277fba963c403c907364fe1e26d08122326
Hopes15 <166359924+Hopes15@users.noreply.github.com>
2025-01-29 14:49:11 +0900

    Utilitiesの追加

---
+++ Utilities.h
@@ -0,0 +1,12 @@
+#pragma once
+
+/// <summary>
+/// 元データを指定した数値でアライメントする
+/// </summary>
+/// <param name="size">			  アラインする対象		</param>
+/// <param name="alignmentSize">　指定アライメントサイズ	</param>
+/// <returns>					　アライメント後のサイズ	</returns>
+inline size_t AlignBufferSize(size_t size, size_t alignmentSize)
+{
+	return size + alignmentSize - size % alignmentSize;
+}
